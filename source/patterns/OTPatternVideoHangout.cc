1f3560784c2f78d4bf2c651e3ae46f3753cf6611
pjuke <e.mathiasson@gmail.com>
2014-12-09 22:01:30 +0100

    fix

--- source/patterns/OTPatternVideoHangout.cc
+++ source/patterns/OTPatternVideoHangout.cc
@@ -163,7 +163,7 @@ static inline void _findListenerDest(
 {
 	//***
 	// Adjust consumers
-	//nConsumers = (nConsumers < 2 ) ? nConsumers = 1 : nConsumers -= 1;
+	nConsumers = (nConsumers < 2 ) ? 1 : nConsumers-1;
 
 	const size_t nActiveParticipants = TSK_MAX(nConsumers, kMinActiveParticipants4SizeCompute);
 	const size_t nListenerWidth = (nMixFrameWidth / nActiveParticipants);
@@ -413,8 +413,8 @@ OTObjectWrapper<OTFrameVideo *> OTPatternVideoHangout::mix(std::map<uint64_t, OT
 	bool layoutChanged = false;
 	std::deque<std::string> myVector;
 	size_t mixCount = 0;
-	OT_DEBUG_WARN("CONSUMERS COUNT:");
-	OT_DEBUG_WARN(nConsumers);
+	//OT_DEBUG_WARN("CONSUMERS COUNT:");
+	//OT_DEBUG_WARN(nConsumers);
 
 	for(iter = pConsumers->begin(), i = 0; iter != pConsumers->end(); ++iter, ++i, bIsSpeaker = false)
 	{
