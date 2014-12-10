1658353af407427f18334db3899825c2a6360fe3
Kalle <kalle.johnson@gmail.com>
2014-12-10 01:56:03 +0100

    Debug msgs.

--- source/OTEngine.cc
+++ source/OTEngine.cc
@@ -1217,6 +1217,8 @@ bool OTEngine::setConfFile(const char* pcConfFileFullPath)
 				if(tsk_striequals("server", pcParamName))
 				{
 					setNodeURI(pcParamValue);
+				}else{
+					std::cout << "server not param name" << std::endl;
 				}
 				// else if(tsk_striequals("server_port", pcParamName))
 				// {
